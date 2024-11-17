2f2ea513a30e6a13b35276ad3c46338b6191763f
LEANDRO MELO OLIVEIRA <leandromelodia@gmail.com>
2024-11-17 20:58:47 -0300

    Cors config

--- src/main/java/com/aleluiaeventos/dslist/controllers/GameListController.java
+++ src/main/java/com/aleluiaeventos/dslist/controllers/GameListController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.aleluiaeventos.dslist.dto.GameListDTO;
 import com.aleluiaeventos.dslist.dto.GameMinDTO;
+import com.aleluiaeventos.dslist.dto.ReplacementDTO;
 import com.aleluiaeventos.dslist.services.GameListService;
 import com.aleluiaeventos.dslist.services.GameService;
 
@@ -29,7 +32,6 @@ public class GameListController {
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
         return result;
-        
     }
 
      @GetMapping(value = "/{listId}/games")
@@ -38,5 +40,11 @@ public class GameListController {
         return result;
         
     }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void mov(@PathVariable Long listId, @RequestBody ReplacementDTO bory){
+        gameListService.move(listId, bory.getSourceIndex(), bory.getDestinationIndex());
+     
+ }
 
 }
--- src/main/java/com/aleluiaeventos/dslist/services/GameListService.java
+++ src/main/java/com/aleluiaeventos/dslist/services/GameListService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 
 import com.aleluiaeventos.dslist.dto.GameListDTO;
 import com.aleluiaeventos.dslist.entities.GameList;
+import com.aleluiaeventos.dslist.projection.GameMinProjection;
 import com.aleluiaeventos.dslist.repositories.GameListRepository;
+import com.aleluiaeventos.dslist.repositories.GameRepository;
+
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -20,13 +23,42 @@ public class GameListService {
     @Autowired
     public GameListRepository gameListRepository;
 
+    @Autowired
+	private GameRepository gameRepository;
+
+	
+
     
     
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
        List<GameList> result = gameListRepository.findAll();
        return result.stream().map(GameListDTO::new)
-                 .collect(Collectors.toList());    
+                 .collect(Collectors.toList());
+    }  
+
+
+                 
+    @Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getID(), i);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public GameListDTO findById(Long id) {
+		GameList entity = gameListRepository.findById(id).get();
+		return new GameListDTO(entity);             
     }
 
 }
